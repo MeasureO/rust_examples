17ee743ba9bc6ef039c2e29bb8eeab6206773a24
MeasureO <42300157+MeasureO@users.noreply.github.com>
2022-08-03 03:18:18 +0300

    Create printing_styles.rs

---
+++ printing_styles.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // one line
+    print!("Rust Programming");
+    print!(" Course");
+    // two lines
+    println!("Rust Programming");
+    println!("Course");
+    // error one line
+    eprint!("Rust Programming");
+    eprint!(" Course");
+    // error two lines
+    eprintln!("Rust Programming");
+    eprintln!("Course");
+}
