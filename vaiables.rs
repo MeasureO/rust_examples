a498f105204ac4fafea4d77135880325ddc70475
MeasureO <42300157+MeasureO@users.noreply.github.com>
2022-08-03 03:31:04 +0300

    Update vaiables.rs

--- vaiables.rs
+++ vaiables.rs
@@ -6,4 +6,15 @@ fn main() {
 
     let (course,category) = ("Rust","beginner"); // assign multiple values
     println!("This is a {} course in {}.", category, course); // print the value
+    
+
+  let outer_variable = 112;
+  {
+        let inner_variable = 213;
+        println!("block variable inner: {}", inner_variable);
+        let outer_variable = 117; // SHADOWING
+        println!("block variable outer: {}", outer_variable);
+  }
+    // ERROR: println!("inner variable: {}", inner_variable); // use of inner_variable outside scope
+
 }
