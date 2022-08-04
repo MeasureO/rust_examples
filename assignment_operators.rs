48cd010e93ccca22a9116a41d707311f71686b70
MeasureO <42300157+MeasureO@users.noreply.github.com>
2022-08-04 03:34:18 +0300

    Create assignment_operators.rs

---
+++ assignment_operators.rs
@@ -0,0 +1,16 @@
+fn main() {
+    //define a mutable variable
+    let mut a = 2;
+    println!("a:{}", a);
+    a += 1;
+    println!("a+=1:{}", a);
+    println!("a:{}", a);
+    a -= 1;
+    println!("a-=1:{}", a);
+    println!("a:{}", a);
+    a /= 1;
+    println!("a/=1:{}", a);
+    println!("a:{}", a);
+    a *= 3;
+    println!("a*=3:{}", a);
+}
