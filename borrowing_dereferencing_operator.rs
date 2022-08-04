3a0dc08423e677ef6da1b0b946a3182636c47ea0
MeasureO <42300157+MeasureO@users.noreply.github.com>
2022-08-04 03:46:24 +0300

    Update borrowing_dereferencing_operator.rs

--- borrowing_dereferencing_operator.rs
+++ borrowing_dereferencing_operator.rs
@@ -12,4 +12,16 @@ fn main() {
     *b = 11; // derefencing 
     println!("Value of b:{}", b); // updated value of b
     println!("Value of y:{}", y); // y value can be changed as it is mutuably borrowed
+    
+    
+    
+        //mutable reference to a variable
+    let mut x = 10;
+    println!("Value of x:{}", x);
+    let a = & mut x;
+    println!("Value of a:{}", a);
+    //dereference a variable
+    *a = 11;
+    println!("Value of a:{}", a);
+    println!("Value of x:{}", x); // Note that value of x is updated
 }
