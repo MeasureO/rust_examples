8eb3f20219cfce0411da271da65f5758f60c8fb1
MeasureO <42300157+MeasureO@users.noreply.github.com>
2022-08-06 04:27:52 +0300

    Create match_expression.rs

---
+++ match_expression.rs
@@ -0,0 +1,26 @@
+fn main() {
+    // define a variable 
+    let x = 5;
+    // define match expression
+    match x {
+        1 => println!("Java"),
+        2 => println!("Python"),
+        3 => println!("C++"),
+        4 => println!("C#"),
+        5 => println!("Rust"),
+        6 => println!("Kotlin"),
+        _ => println!("Some other value"),
+    };
+
+   // define a variable
+   let course = "Rust";
+   // return value of match expression in a variable
+   let found_course = match course {
+      "Rust" => "Rust",
+      "Java" => "Java",
+      "C++" => "C Plus Plus",
+      "C#" => "C Sharp",
+      _ => "Unknown Language"
+   };
+   println!("Course name : {}",found_course);
+}
