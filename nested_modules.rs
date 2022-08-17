ac43f591ba827608c0e0e106431537f8e736cec5
MeasureO <42300157+MeasureO@users.noreply.github.com>
2022-08-17 04:59:27 +0300

    Create nested_modules.rs

---
+++ nested_modules.rs
@@ -0,0 +1,12 @@
+pub mod chapter {
+    pub mod lesson { // mod level 1
+        pub mod heading { // mod level 2
+            pub fn illustration() {  // mod level 3
+              println!("Hi, I'm a 3rd level nested function");
+            }
+        }
+    }
+}
+fn main() {
+    chapter::lesson::heading::illustration(); // call the function
+}
