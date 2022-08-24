38922d1ddff6a4bf79f007b780d8a9b9e04c1ef7
MeasureO <42300157+MeasureO@users.noreply.github.com>
2022-08-24 23:01:55 +0300

    Create borrowing_ex3.rs

---
+++ borrowing_ex3.rs
@@ -0,0 +1,14 @@
+// 'a' an owner variable
+// 'b' a shared borrow
+// 'c' a mutable borrow
+fn example(a: i32, b:& i32,c : &mut i32){
+  println!("a: {}, b: {}, c: {}", a , b , c);
+   *c=9;
+}
+fn main(){
+   let a = 1;
+   let b = 2;
+   let mut c = 3;
+   example( a, &b , &mut c);
+   println!("a: {}, b: {}, c: {}", a , b , c);
+}
