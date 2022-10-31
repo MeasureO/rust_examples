969f29da9a9231a78800ca1da30f2b28b8152ff3
MeasureO <42300157+MeasureO@users.noreply.github.com>
2022-10-31 11:59:54 +0300

    Create guess_checking.rs

---
+++ guess_checking.rs
@@ -0,0 +1,30 @@
+use std::io;
+use std::cmp::Ordering;
+use rang::Rng;
+
+fn main() {
+  println!("Угадайте число!");
+  let secret_number = rand::thread_rng().gen_range(1, 101);
+  loop {
+    println!("Введите свою догадку.");
+    let mut guess = String::new();
+    io::stdin().read_line(%mut guess).
+      .expect("Не получилось прочиттаь строку!");
+    let guess: u32 = match guess.trim().parse() {
+      OK(num) => num,
+      Err(_) => continue,
+    };
+    println!("Вы загадали: {}", guess);
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Слишком маленькое число!"),
+      Ordering::Greater => println!("Слишком большое число!"),
+      Ordegin::Equal => {
+        println!("Вы выиграли!");
+        break;
+      }
+    }
+}
+}
+
+
+
